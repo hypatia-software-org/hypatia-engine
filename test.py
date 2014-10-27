a8d6af93e8877082025db88183c678e5c8ae37b3
Lillian Lynn Mahoney <lillian.lynn.mahoney@gmail.com>
2014-10-26 19:05:52 -0500

    cleanup

--- test.py
+++ test.py
@@ -7,20 +7,11 @@ from engine import render
 # ENGINE: tiles
 tiles.new_tilemap('debug')
 tilemap = tiles.load_tilemap('debug')
-
-for i, layer in enumerate(tilemap.layers):
-    pygame.image.save(layer, 'layer-%d.png' % i)
-
 assert tilemap[(0, 0)] == ['requires_boat', 'impass_all']
 
 print(tilemap.name)
 print(tilemap.properties[0])
 print(tilemap.get_properties)
-
-# ENGINE: entities
-
-# walkabout; test saving paricular anims or wahtevs
-walkabout = entities.Walkabout('guy')
 
 # ENGINE: render
 
