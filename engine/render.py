179b589e2ad276c5920ceef90315b2c435b41ba3
Lillian Lynn Mahoney <lillian.lynn.mahoney@gmail.com>
2014-10-27 21:20:25 -0500

    fixed animation bug: timing conversion error

--- engine/render.py
+++ engine/render.py
@@ -120,7 +120,7 @@ def gif_to_pyganim(gif_path):
         while 1:
             # must find frame time, create a pygame surface from frame
             gif.putpalette(palette)
-            duration = gif.info['duration'] / 1000
+            duration = gif.info['duration'] / 1000.0
             frame_as_pygame_image = pil_to_pygame(gif, "RGBA")
             frames.append((frame_as_pygame_image, duration))
             frame_index += 1
