38e982439b71c0fc0b982df826a8c4cc8e135694
Lillian Lemmer <lillian.lynn.lemmer@gmail.com>
2015-06-09 18:34:44 -0500

    Quickstart, added install scripts.

---
+++ install-linuxmint-34.sh
@@ -0,0 +1,19 @@
+# Hypatia installation script for Linuxmint and Python 3.4.
+#
+# Forgot which version this was tested in.
+
+# install depends
+sudo apt-get install python3-pip python3-dev libsdl1.2-dev\
+     libsdl-image1.2-dev libsdl-mixer1.2-dev libsdl-ttf2.0-dev\
+     libsdl-gfx1.2-dev libsdl-net1.2-dev libsdl-image1.2-dev\
+     libsdl-mixer1.2-dev libsdl-ttf2.0-dev libsmpeg-dev\
+     libsdl1.2-dev libportmidi-dev libswscale-dev libavformat-dev\
+     libavcodec-dev libsdl-sge-dev libsdl-sound1.2-dev\
+     libportmidi-dev libsmpeg-dev
+pip3 install --user Image pyganim
+
+# compile pygame
+hg clone https://bitbucket.org/pygame/pygame
+cd pygame
+python3 setup.py build
+python3 setup.py install --user
