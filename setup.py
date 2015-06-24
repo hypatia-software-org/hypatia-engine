97ded2e65a5e5802e47af688f52c8910ced4389d
Lillian Lemmer <lillian.lynn.lemmer@gmail.com>
2015-06-24 04:34:08 -0500

    Updated version to 0.2.3--using hypatia_engine on PyPI since hypatia is unavail--for now

--- setup.py
+++ setup.py
@@ -2,13 +2,12 @@
 
 """Hypatia package installer.
 
-Notes:
-  $ setup.py sdist bdist_wheel
-  $ twine upload dist/hypatia-0.2.2.tar.gz dist/hypatia-0.2.2*.whl
-  $ rm -rf dist
+$ setup.py sdist bdist_wheel
+$ twine upload dist/hypatia_engine-0.2.3.tar.gz dist/hypatia_engine-0.2.3*.whl
+$ rm -rf dist
 
-  You'll need the wheel, twine package for bdist_wheel. Don't forget
-  to clear your dist when finished.
+You'll need the wheel, twine package for bdist_wheel. Don't forget
+to clear your dist when finished.
 
 """
 
@@ -16,9 +15,9 @@ from setuptools import setup
 
 
 exec(open('hypatia/version.py').read())
-setup(name='hypatia',
+setup(name='hypatia_engine',
       version=__version__,
-      description='2D adventure game engine',
+      description='2D action adventure game engine',
       author='Lillian Lemmer',
       author_email='lillian.lynn.lemmer@gmail.com',
       url='http://lillian-lemmer.github.io/hypatia',
