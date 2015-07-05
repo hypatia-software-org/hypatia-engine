286058a834e548af530e1744445a288272ca4e2d
Lillian Lemmer <lillian.lynn.lemmer@gmail.com>
2015-07-05 01:55:33 -0500

    Sphinx updates

--- make-sphinx.sh
+++ make-sphinx.sh
@@ -3,6 +3,8 @@
 # Create sphinx api docs.
 
 # lazily slapped together, will come back to improve
+test.sh
 rm -rf api
 sphinx-apidoc -f -o sphinx-source/ hypatia
 sphinx-build sphinx-source api
+firefox api/index.html
