e7171efbc3fbe99dd6e056883830447bf508b4c5
Lillian Lemmer <lillian.lynn.lemmer@gmail.com>
2015-07-05 13:25:11 -0500

    Merged pull request #45 and modified it.

--- hypatia/actor.py
+++ hypatia/actor.py
@@ -3,22 +3,22 @@
 
 """Implementation of actors.
 
-Actors are the base of any entities which may perform actions.
-Examples of actors are enemies, NPCs, the player herself, etc.  This
-module implements a basic ``Actor`` class to serve as the parent for any
-classes representing objects such as those examples.  Any logic which
-can be shared by all such objects belongs in the class, for example
-the logic for moving around the game world.  This approach makes it
-possible to allow, for example, enemies and players to share much of
-the same behavior, and this can be useful by making it easier (from a
+Actors are the base of any entities which may perform actions. Examples
+of actors are enemies, NPCs, the player themselves, etc. This module
+implements a basic :class:`Actor` class to serve as the parent for any
+classes representing objects such those examples. Any logic which can
+be shared by all objects belongs in the class, for example, the logic
+for moving around the game world. This approach makes it possible to
+allow, for example, enemimes and players to share much of the same
+behavior, a nd this can be useful by making it easer (from a
 programming point-of-view) to give monsters the same set of core
 abilities and logic as NPCs, etc.
 
-When type-checking is necessary the ``Actor`` class provides a useful
-way to test for objects which support a bare-minimum of core, shared
-actions.  The class is also useful in role-playing games for storing
-data that tends to be common between the Player, NPCs, enemies, et
-alia, a common example being statistics like hit-points.
+When type-checking is necessary the :class:`Actor` class provides a
+useful way to test for objects which support a bare-minimum of core,
+shared actions.  The class is also useful in role-playing games for
+storing data that tends to be common between the Player, NPCs, enemies,
+etalia, a common example being statistics like hit-points.
 
 """
 
@@ -30,17 +30,18 @@ from hypatia import constants
 class Actor(object):
     """The base class for any entity which can perform actions.
 
-    For example, both ``Player`` and ``NPC`` objects can move around
-    the game world.  This is the type of action which is shared by all
-    "actors" and therefore best implemented in this class, allowing it
-    to be shared by as many entities as possible, e.g. enemies.
+    For example, both :class:`player.Player` and :class:`player.NPC`
+    objects can move around the game world. This is the type of action
+    which is shared by all "actors" and therefore best implemented in
+    this class, allowing it to be shared by as many entities as
+    possible, e.g. enemies.
 
-    It is typically not useful to directly instantiate ``Actor``
+    It is typically not useful to directly instantiate :class:`Actor`
     objects but the implementation does not prevent this.
 
     Attributes:
 
-        walkabout: An instance of ``animations.Walkabout``.
+        walkabout: An instance of :class:`animations.Walkabout`.
 
     """
 
@@ -49,24 +50,24 @@ class Actor(object):
 
         Arguments:
 
-            walkabout: An instance of ``animations.Walkabout``, which
-                is then accessible via the ``walkabout`` property.
+            walkabout: An instance of :class:`animations.Walkabout`,
+                which is then accessible via the ``walkabout`` property.
                 This argument is optional and defaults to new instance
-                of ``animations.Walkabout``.
+                of :class:`animations.Walkabout`.
 
         """
         self.walkabout = walkabout or animations.Walkabout()
 
         @property
         def direction(self):
-            """An intsance of ``constants.Direction``
+            """An intsance of :class:`constants.Direction`
 
             This property indicates the direction the actor is facing.
             Is it possible to set this property to a new value.
 
             Raises:
                 AttributeError: If the new value is not a valid object
-                    of the ``constants.Direction`` class.
+                    of the :class:`constants.Direction` class.
 
                 TypeError: If one tries to delete this property
 
@@ -78,7 +79,8 @@ class Actor(object):
 
             if not isinstance(new_direction, constants.Direction):
 
-                raise AttributeError(("Direction must be a valid constants.Direction value"))
+                raise AttributeError(("Direction must be a valid "
+                                      "constants.Direction value"))
 
             else:
                 self.walkabout.direction = new_direction
