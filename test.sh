1fd2066a701e118f13a03af476dec5c3e203e112
Lillian Lemmer <lillian.lynn.lemmer@gmail.com>
2015-07-04 23:05:56 -0500

    Made a really stupid mistake of installing hypatia_engine in the test instead of using setup.py/. (current directory).

--- test.sh
+++ test.sh
@@ -3,5 +3,5 @@
 # How I test before making a commit.
 
 pip uninstall hypatia_engine -y
-pip install --user hypatia_engine
+pip install --user --no-cache-dir .
 py.test tests --pep8 --doctest-modules hypatia -v --cov-report term-missing --cov=hypatia | more
