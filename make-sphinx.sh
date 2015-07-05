6f44f5b094624b43115ff7c95ced03e8ecb5763c
Lillian Lemmer <lillian.lynn.lemmer@gmail.com>
2015-07-05 11:33:36 -0500

    Updating readme. Had error in make-sphinx.

--- make-sphinx.sh
+++ make-sphinx.sh
@@ -3,7 +3,7 @@
 # Create sphinx api docs.
 
 # lazily slapped together, will come back to improve
-test.sh
+./test.sh
 rm -rf api
 sphinx-apidoc -f -o sphinx-source/ hypatia
 sphinx-build sphinx-source api
