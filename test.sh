b1e9bb4e754d9e57f90a5c514ae72abff75b28c7
Lillian Lemmer <lillian.lynn.lemmer@gmail.com>
2015-07-12 23:45:57 -0500

    pypi rst parsing is meh

--- test.sh
+++ test.sh
@@ -6,6 +6,7 @@
 
 pip uninstall hypatia_engine -y
 pandoc README.md -t rst -o PKG-INFO
+python setup.py check -r -s
 pip install --user --no-cache-dir .
 rm PKG-INFO
 py.test tests --pep8 --doctest-modules hypatia -v --cov-report term-missing --cov=hypatia | more
