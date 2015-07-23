f1229b053f33c3dc22490df326c54c56d49c9842
Lillian Lemmer <lillian.lynn.lemmer@gmail.com>
2015-07-23 18:14:46 -0500

    This branch solved issue #48.

--- install-base-python2.sh
+++ install-base-python2.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 #
-# Any commands which can be used to install dependencies across ALL
-# supported BSD and Linux (maybe even Mac?) platforms.
+# This script file is used by all Python 2 installation
+# scripts, because it is assumed that "pip" refers to a
+# Python 2 installation, considering we're installing
+# for Python 2.
 #
-# This script is used by other python2 install scripts.
+# This script uses pip to install two requirements files
+# and the setup.py for Hypatia. The --user flag installs
+# the package to the user's site packages.
+#
+# Does not require root.
 
 pip install --user -r requirements/python2.txt .
-
--- test.sh
+++ test.sh
@@ -1,8 +1,34 @@
 #!/bin/sh
 #
-# How I test before making a commit.
+# This script is STRONGLY recommended for verifying the quality and
+# functionality of your contribution.
 #
-# Not used for travis. because this launches the demo therafter
+# This script can be ran as a pre-push git hook; please see the etc/
+# directory in this project's root.
+#
+# Not used for Travis CI, because this launches the demo game.
+#
+# Also checks the validity of the markdown-to-PyPI-rst conversion
+# performed on README.md. This is done in order to provide PyPi
+# with a nice package page/read me.
+#
+# CAVEAT: If you get an error about --no-cache-dir,
+# you need to upgrade pip.
+#
+# This script does the following:
+#   1. Uninstall the current hypatia_engine Python package
+#   2. convert README.md to PKG-INFO (rst)
+#   3. Validate PKG-INFO, if it doesn't validate it won't be
+#      formatted on PyPi
+#   4. Install the local repo's package for Hypatia as user,
+#      using pip and setup.py.
+#   5. Remove the PKG-INFO file (we were only validating it)
+#   6. Use py.test to check for PEP8 rule violations, to
+#      doctest all docstrings, provide a test coverage
+#      report and pipe all of this information to MORE
+#   7. The demo game.py will be launched, utilizing the
+#      freshly installed Hypatia package from this local
+#      repository.
 
 pip uninstall hypatia_engine -y
 pandoc README.md -t rst -o PKG-INFO
