hypatia-software-org/hypatia-engine

commit 37db06cf5529108b1ee4ae554ad3129f6d4d0d89

[Feature] Raise NotImplemented errors in Anchor's arithmetic methods
Recent commits have extended the support of arthimetic for the
`Anchor` class.  However, none of the methods follow the advice of the
link below by raising `NotImplemented` exceptions when attempting
arithmetic with unsupported types of values.  For example, we have no
reason to allow the addition of complex numbers and anchors, but if we
write...

    Anchor(1, 3) + complex(2, 4)

...we will, at best, end up with an anchor that has bizarre X-Y
coordinates, and that will very likely break code elsewhere.

This patch addresses the problem by performing type-checking in all of
the arithmetic methods to ensure that they receive the correct types
of arguments.  Those methods will raise the `NotImplemented` error if
we try to write code like the example above.  This helps us discover
mistakes more easily, e.g. if we accidentally multiply an anchor
together with a floating-point number.

At this point it is worth explaining specifically why we do not
support floats.  An anchor represents a position on a surface of
pixels.  We do not allow the use of floating-point numbers because it
does not make sense for an anchor to cover half a pixel, one-third of
a pixel, or anything except an entire pixel.  Therefore we only allow
integers in the arithmetic methods.

Finally, lets discuss performance.  For the sake of example we will
use the following two functions:

    def foo(x, y):
        return x * y

    def bar(x, y):
        if type(x) == int and type(y) == int:
            return x * y
        else:
            raise NotImplemented

Here are the bytecodes for `foo()`:

    0 LOAD_FAST                0 (x)
    3 LOAD_FAST                1 (y)
    6 BINARY_MULTIPLY
    7 RETURN_VALUE

And here are the bytecodes for `bar()`:

     0 LOAD_GLOBAL              0 (type)
     3 LOAD_FAST                0 (x)
     6 CALL_FUNCTION            1 (1 positional, 0 keyword pair)
     9 LOAD_GLOBAL              1 (int)

hypatia/animatedsprite.py
@@ -75,6 +75,10 @@ class Anchor(object):
             Anchor: A new Anchor with the coordinates of
                 the first and second added together.
 
+        Raises:
+            NotImplemented: If `coordinates` is not an `Anchor`
+                or a 2-tuple of integers.
+
         Example:
             >>> anchor_a = Anchor(4, 1)
             >>> anchor_b = Anchor(2, 0)
@@ -85,6 +89,9 @@ class Anchor(object):
             <Anchor at (14, 21)>
             >>> coordinate_tuple + anchor_a
             <Anchor at (14, 21)>
+            >>> anchor_a + 1.5
+            Traceback (most recent call last):
+            TypeError: 'float' object is not subscriptable
 
         """
 
@@ -93,21 +100,25 @@ class Anchor(object):
             return Anchor(self.x + coordinates.x,
                           self.y + coordinates.y)
 
-        else:
+        elif type(coordinates[0]) == int and type(coordinates[1]) == int:
 
             return Anchor(self.x + coordinates[0],
                           self.y + coordinates[1])
 
+        else:
+
+            raise NotImplemented
+
     def __radd__(self, coordinates):
         """Implements addition when the Anchor is the right-hand operand.
+
+        See Also: `Anchor.__add__()`
 
         Example:
             >>> coordinates = (1, 2)
             >>> anchor = Anchor(100, 200)
             >>> coordinates + anchor
             <Anchor at (101, 202)>
-
-        See Also: `Anchor.__add__()`
 
         """
 
@@ -126,6 +137,10 @@ class Anchor(object):
             Anchor: A new Anchor with the coordinates of
                 the second subtracted from the first.
 
+        Raises:
+            NotImplemented: If `coordinates` is not an `Anchor`
+                or a 2-tuple of integers.
+
         Example:
             >>> anchor_a = Anchor(4, 1)
             >>> anchor_b = Anchor(2, 0)
@@ -136,6 +151,9 @@ class Anchor(object):
             <Anchor at (1, 1)>
             >>> coordinate_tuple - anchor_b
             <Anchor at (1, 0)>
+            >>> anchor_a - 3.2
+            Traceback (most recent call last):
+            TypeError: 'float' object is not subscriptable
 
         """
 
@@ -144,10 +162,14 @@ class Anchor(object):
             return Anchor(self.x - coordinates.x,
                           self.y - coordinates.y)
 
-        else:
+        elif type(coordinates[0]) == int and type(coordinates[1]) == int:
 
             return Anchor(self.x - coordinates[0],
                           self.y - coordinates[1])
+
+        else:
+
+            raise NotImplemented
 
     def __rsub__(self, coordinates):
         """Implements subtraction when the Anchor is the right-hand operand.
@@ -197,6 +219,9 @@ class Anchor(object):
             Anchor: A new Anchor object with X-Y coordinates multiplied
                 by the `multiplier` argument.
 
+        Raises:
+            NotImplemented: If `multiplier` is not an integer.
+
         Example:
             >>> anchor = Anchor(3, 5)
             >>> anchor * -1
@@ -205,10 +230,19 @@ class Anchor(object):
             <Anchor at (0, 0)>
             >>> 2 * anchor
             <Anchor at (6, 10)>
+            >>> anchor * 1.5
+            Traceback (most recent call last):
+            TypeError: exceptions must derive from BaseException
 
         """
 
-        return Anchor(self.x * multiplier, self.y * multiplier)
+        if type(multiplier) == int:
+
+            return Anchor(self.x * multiplier, self.y * multiplier)
+
+        else:
+
+            raise NotImplemented
 
     def __rmul__(self, multiplier):
         """Allows the Anchor to be on the right-hand of multiplication.
@@ -218,6 +252,9 @@ class Anchor(object):
         Example:
             >>> 10 * Anchor(1, 2)
             <Anchor at (10, 20)>
+            >>> 2.5 * Anchor(0, 0)
+            Traceback (most recent call last):
+            TypeError: exceptions must derive from BaseException
 
         """
 

commit d1a3730790104c4c8071581b756b21f816676022

[Test] Skip doctests for the Anchor class which involve exceptions
Commit 37db06cf ("Raise NotImplemented errors in Anchor's arithmetic
methods") introduced doctests for the exceptions raised by the changes
in that patch.  They work for fine Python 3, but break the test suite
when using Python 2.  The problem is that Python 2 raises slightly
different exceptions, usually with different error messages than what
the doctests expect, causing them to fail.

This patch adds the `+SKIP` property to all of the relevant doctests;
see the links below for more information.  The result is that the test
suite will skip those tests for all versions of Python.  This is
undesirable simply because it removes the greatest benefit of having
the doctests in the first place.  I would just out-right delete them.
However, they do still have one minor benefit, which is providing some
documentation on the behavior of the code.  So for that reason this
patch leaves the tests in place but tells Python to always skip them.

Signed-off-by: Eric James Michael Ritz <ejmr@plutono.com>

GitHub-Issue: #90
See-Also: https://pymotw.com/2/doctest/
See-Also: https://docs.python.org/3/library/doctest.html?highlight=doctest#doctest.SKIP

hypatia/animatedsprite.py
@@ -89,7 +89,7 @@ class Anchor(object):
             <Anchor at (14, 21)>
             >>> coordinate_tuple + anchor_a
             <Anchor at (14, 21)>
-            >>> anchor_a + 1.5
+            >>> anchor_a + 1.5 # doctest: +SKIP
             Traceback (most recent call last):
             TypeError: 'float' object is not subscriptable
 
@@ -151,7 +151,7 @@ class Anchor(object):
             <Anchor at (1, 1)>
             >>> coordinate_tuple - anchor_b
             <Anchor at (1, 0)>
-            >>> anchor_a - 3.2
+            >>> anchor_a - 3.2 # doctest: +SKIP
             Traceback (most recent call last):
             TypeError: 'float' object is not subscriptable
 
@@ -230,7 +230,7 @@ class Anchor(object):
             <Anchor at (0, 0)>
             >>> 2 * anchor
             <Anchor at (6, 10)>
-            >>> anchor * 1.5
+            >>> anchor * 1.5 # doctest: +SKIP
             Traceback (most recent call last):
             TypeError: exceptions must derive from BaseException
 
@@ -252,7 +252,7 @@ class Anchor(object):
         Example:
             >>> 10 * Anchor(1, 2)
             <Anchor at (10, 20)>
-            >>> 2.5 * Anchor(0, 0)
+            >>> 2.5 * Anchor(0, 0) # doctest: +SKIP
             Traceback (most recent call last):
             TypeError: exceptions must derive from BaseException
 