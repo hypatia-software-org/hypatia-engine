a8a1553d94eb49ffa16f6f2692723d337ca36217
Eric James Michael Ritz <ejmr@plutono.com>
2015-10-21 22:31:27 -0400

    [Test] Add unit tests for the NPC class
    This patch adds a new file, `tests/test_player.py`, which contains
    unit tests for the `NPC` class, intended for use with pytest.  One of
    the unit tests requires the `unittest.mock` package in order to test
    whether or not certain methods were called as expected.  That package
    is part of the standard library in Python 3.4 and later.  However, in
    order to support Python 2.7 we must add the `mock` package to the list
    of requirements.  In order to use the package in conjunction with
    pytest we must also install the `pytest-mock` module.  So this patch
    updates the appropriate files in the `requirements/` directory so that
    the test process will install both packages as needed.
    
    Signed-off-by: Eric James Michael Ritz <ejmr@plutono.com>
    
    GitHub-Issue: #59
    See-Also: https://pypi.python.org/pypi/mock
    See-Also: https://github.com/pytest-dev/pytest-mock

---
+++ tests/test_player.py
@@ -0,0 +1,43 @@
+# This module is part of Hypatia and is released under the
+# MIT license: http://opensource.org/licenses/MIT
+
+"""Unit tests for the `hypatia/player.py` code."""
+
+import pytest
+from unittest.mock import patch
+
+from hypatia import player
+
+
+class TestNPC(object):
+    """Unit tests for the player.NPC class."""
+
+    def test_inactive_by_default(self):
+        """Asserts that NPCs are not active by default."""
+        npc = player.NPC()
+        assert npc.active is False
+
+    def test_cannot_delete_active_property(self):
+        """Asserts that trying to delete the `active` property of an NPC
+        raises a TypeError.
+
+        """
+        npc = player.NPC()
+        with pytest.raises(TypeError):
+            del npc.active
+
+    def test_setting_active_invokes_methods(self):
+        """Asserts that setting the `active` property of an NPC to True will
+        invoke its on_activation() method, and likewise, setting the
+        property to False invokes the on_deactivation() method.
+
+        """
+        with patch.object(player.NPC, "on_activation") as mock_method:
+            npc = player.NPC()
+            npc.active = True
+            mock_method.assert_called_once_with()
+
+        with patch.object(player.NPC, "on_deactivation") as mock_method:
+            npc = player.NPC()
+            npc.active = False
+            mock_method.assert_called_once_with()
