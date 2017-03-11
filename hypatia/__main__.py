a874da11069751542d666f8fe7fbbcd04de524b3
Alice Jenkinson <h@nea.nz>
2017-03-11 18:33:07 +1300

    Add a better launch script, tidy up setup.py

--- hypatia/__main__.py
+++ hypatia/__main__.py
@@ -1,7 +1,34 @@
-import os
+"""Hypatia 2D game engine.
 
+Usage:
+    hypatia run <path> [options]
+    hypatia new-game <path> [options]
+
+Options:
+    --version    Display Hypatia version and exit.
+    --help       Display this help notice and exit.
+
+"""
+
+import os
+import docopt
+
+from hypatia import __version__
 from hypatia.game import Game
 
 
-game = Game(os.getcwd())
-game.run()
+def main():
+    args = docopt.docopt(__doc__, version=f"Hypatia Engine {__version__}")
+
+    if args['run']:
+        game = Game(args['<path>'])
+        game.run()
+
+    elif args['new-game']:
+        raise NotImplementedError("No functionality to create new games exists yet.")
+
+    else:
+        print("What do you want me to do?")
+
+if __name__ == "__main__":
+    main()
--- setup.py
+++ setup.py
@@ -1,57 +1,57 @@
 #!/usr/bin/env python
 
-"""Hypatia package installer.
-
-Distributing:
-
-  $ setup.py sdist bdist_wheel
-  $ twine upload dist/hypatia_engine-0.2.3.tar.gz dist/hypatia_engine-0.2.3*.whl
-  $ rm -rf dist
-
-  You'll need the wheel, twine package for bdist_wheel. Don't forget
-  to clear your dist when finished.
-
-Installing:
-  `pip install .` in the project root. This script detects the
-  python version and builds the install_requires accordingly.
-
-  Does not install pygame. See `README.md`.
-
-"""
-
 import sys
 from setuptools import setup
 from distutils.version import StrictVersion
 
-install_requires = ['Pillow>=2']
+# figure out python version and bail if we're not >3.6
+python_version = StrictVersion('.'.join(str(n) for n in sys.version_info[:3]))
+if python_version < StrictVersion('3.6'):
+    raise RuntimeError("Hypatia Engine requires at least Python version 3.6.")
 
 exec(open('hypatia/__init__.py').read())
-setup(name='hypatia_engine',
-      packages=['hypatia'],
-      version=__version__,
-      description='2D action adventure game engine',
-      setup_requires=['setuptools-markdown'],
-      # pygame isn't on pypi
-      # TODO: i should maybe also specifiy Pillow<4
-      install_requires=install_requires,
-      long_description_markdown_filename='README.md',
-      author='Hypatia Software Organization',
-      author_email='contact@hypatiasoftware.org',
-      url='http://engine.hypatiasoftware.org',
-      download_url = 'https://github.com/hypatia-software-org/hypatia-engine/releases/tag/' + __version__,
-      license='MIT',
-      classifiers=['Development Status :: 3 - Alpha',
-                   'Intended Audience :: Developers',
-                   'Natural Language :: English',
-                   'License :: OSI Approved :: MIT License',
-                   'Programming Language :: Python :: 3.5',
-                   'Operating System :: Microsoft :: Windows',
-                   'Operating System :: POSIX :: BSD :: FreeBSD',
-                   'Operating System :: POSIX :: Linux',
-                   'Topic :: Games/Entertainment :: Role-Playing',
-                   'Topic :: Software Development :: Libraries :: pygame',
-                  ],
-      keywords=('games gaming development sprites adventure game tilemap '
-                'tilesheet zelda gamedev 2d')
-    )
+
+install_requires = [
+    'Pillow>=2',
+    "pygame",
+    "docopt"
+]
+
+setup_args = {
+    "name": 'hypatia_engine',
+    "packages": ['hypatia'],
+    "version": __version__,
+    "description": '2D action adventure game engine',
+    "setup_requires": ['setuptools-markdown'],
+    "install_requires": install_requires,
+    "long_description_markdown_filename": 'README.md',
+    "author": 'Hypatia Software Organization',
+    "author_email": 'contact@hypatiasoftware.org',
+    "url": 'http://engine.hypatiasoftware.org',
+    "download_url": 'https://github.com/hypatia-software-org/hypatia-engine/releases/tag/' + __version__,
+    "license": 'MIT',
+    "entry_points": {
+        "console_scripts": [
+            "hypatia = hypatia.__main__:main"
+        ],
+    },
+    "classifiers": [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX :: BSD :: FreeBSD',
+        'Operating System :: POSIX :: Linux',
+        'Topic :: Games/Entertainment :: Role-Playing',
+        'Topic :: Software Development :: Libraries :: pygame',
+    ],
+    "keywords": (
+        'games gaming development sprites adventure game tilemap '
+        'tilesheet zelda gamedev 2d'
+    ),
+}
+
+setup(**setup_args)
 
