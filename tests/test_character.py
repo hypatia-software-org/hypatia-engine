9467e0dcceea4cf4726fcab15890bc60109d7433
Alice Jenkinson <h@nea.nz>
2017-04-27 19:51:37 +1200

    Fix tests being broken by new interact() function parameter

--- tests/test_character.py
+++ tests/test_character.py
@@ -2,6 +2,7 @@ import os
 import pygame
 
 from hypatia.utils import compare_surfaces
+from hypatia.test_mocks import MockGame
 from hypatia.resources.filesystem import FilesystemResourcePack
 from hypatia.tilesheet import Tilesheet
 from hypatia.tilemap import Tilemap, TilemapTileFlags
@@ -11,10 +12,12 @@ class TestTileNPCCharacter:
         dir_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'testgame', 'resources')
         resourcepack = FilesystemResourcePack(dir_path)
 
+        game = MockGame()
+
         tilemap = Tilemap.from_resource_pack(resourcepack, "testmap")
         tilemap.update(0)
 
-        data = tilemap.tile_data[0][1][1]["tile"].interact()
+        data = tilemap.tile_data[0][1][1]["tile"].interact(game)
 
         assert "say" in data
         assert "test" in data["say"]
